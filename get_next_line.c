670e88616109c209150828f1775edeb6808ce0df
hongfu12321 <hongfu1220@gmail.com>
2018-05-23 20:04:10 -0700

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,48 @@
 /*   By: fhong <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/23 17:53:14 by fhong             #+#    #+#             */
-/*   Updated: 2018/05/23 18:01:22 by fhong            ###   ########.fr       */
+/*   Updated: 2018/05/23 20:03:39 by fhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
+char	*store_str(char *buf, char **line, int index)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 1;
+	while (buf[len] != '\n' && !buf[len])
+		len++;
+	line[index] = ft_strnew(len);
+	while (i < len)
+	{
+		line[index][i] = buf[i];
+		i++;
+	}
+	line[i] = '\0';
+	if (!buf[i])
+		return (&buf[i]);
+	return (&buf[i + 1]);
+}
+
+
 int		get_next_line(const int fd, char **line)
 {
-	char	*buf;
+	static char	*buf;
+	static int	index;
 
-	buf = ft_strnew(BUFF_SIZE);
+	if (!buf)
+	   buf = ft_strnew(BUFF_SIZE * 2);
+	if (!index)
+		index = 0;
 	if (!read(fd, buf, BUFF_SIZE))
-		return (-1);
+		return (0);
+	while (buf = store_str(buf, line, index))
+		index++;
+	return (1);
 
 }
